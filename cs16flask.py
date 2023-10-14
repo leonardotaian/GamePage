694013ce4bd4d0ba3a1acb5f147e8582e18bfd2f
leonardotaian <leonardotaianav@outlook.com>
2023-10-13 22:33:37 -0300

    dando uma estilizada

--- cs16flask.py
+++ cs16flask.py
@@ -1,68 +1,17 @@
 from flask import Flask, render_template
+from flask_login import login_manager
+from flask_sqlalchemy import SQLALchemy
 
-personagens = {
-    1:{
-        "nome": "SAS",
-        "origem": "Escócia",
-        "imagem": "https://www.sitecs.net/images/players/sas_a.jpg"
-    },
-    2: {
-        "nome": "GSG9",
-        "origem": "Alemanha",
-        "imagem": "https://www.sitecs.net/images/players/gsg9_a.jpg"
-    },
-    3: {
-        "nome": "GIGN",
-        "origem": "França",
-        "imagem": "https://www.sitecs.net/images/players/gign_a.jpg"
-    },
-    4: {
-        "nome": "Terror",
-        "origem": "Egito",
-        "imagem": "https://www.sitecs.net/images/players/terror_a.jpg"
-    },
-    5: {
-        "nome": "Arab",
-        "origem": "Palestina",
-        "imagem": "https://www.sitecs.net/images/players/arab_a.jpg"   
-    },
-    6: {
-        "nome": "Guerrila",
-        "origem": "Cuba",
-        "imagem": "https://www.sitecs.net/images/players/gor_a.jpg"   
-    },
-    7: {
-        "nome": "Artic",
-        "origem": "Rússia",
-        "imagem": "https://www.sitecs.net/images/players/arctic_a.jpg"   
-    },
-    8: {
-        "nome": "Seal Team Six",
-        "origem": "EUA",
-        "imagem": "https://www.sitecs.net/images/players/urban_a.jpg"   
-    },
-    9: {
-        "nome": "VIP",
-        "origem": "Inglaterra",
-        "imagem": "https://www.sitecs.net/images/players/vip_a.jpg"   
-    },
-    10: {
-        "nome": "Hostage",
-        "origem": "Desconhecido",
-        "imagem": "https://www.sitecs.net/images/players/hostage1_a.jpg"   
-    }
-}
 
-usuarios = {
-    'nome' : 'leonardo', 'senha' : '1234', 'email' : 'leonardotaianav@outlook.com' 
-}
 
+
+db = SQLALchemy()
 
 app = Flask(__name__)
 
 @app.route("/")
 def cs():
-    return personagens[1]["nome"]
+    return "Eae men kk!"
 
 @app.route("/personagem/<int:personagem_id>")
 def mostra_personagem(personagem_id):
